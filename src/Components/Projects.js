7f8972160ab44b5629494a63b3136d233bff89fd
Larry Motuzis <larrymotuzis@Larrys-MacBook-Pro.local>
2023-07-11 09:32:05 -0500

    update

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,11 +11,11 @@ to: "/",
 video: testBrewDemo,
 description:'TestBrew is a platform that empowers developers to enhance their unit testing skills in just 15 minutes. By engaging in a series of exercises, users can learn to write more effective unit tests. TestBrew evaluates the unit tests written by users by utilizing the abstract syntax tree parsing capabilities of Acorn/Acorn-Walk. This analysis enables TestBrew to provide valuable suggestions and hints to users, helping them improve and rectify their unit tests. The frontend of TestBrew was created using Codemirror and React, ensuring a seamless and user-friendly interface. On the other hand, the backend was developed utilizing Node.js and Express, ensuring a robust and efficient foundation for the platform.'
 },
-{
-name: "Source Code Books",
-to: "/",
-description:'The e-commerce application offers a seamless platform for consumers to effortlessly purchase books. With the ability to search and filter books by category, users can easily find their desired titles. Furthermore, they can conveniently add books to their cart and wish list, proceed to checkout, and review their orders. To enhance the overall experience, customers receive email notifications once their orders have been successfully placed. On the administrative side, the application equips administrators with efficient product and user management functionalities, allowing them to effectively oversee and manage the platform operations. Tech Stack: The application is built using a robust tech stack, including React for the frontend, Node.js and Express for the backend, PotgreSQL for the database, and Node Mailer for email notifications.'
-},
+// {
+// name: "Source Code Books",
+// to: "/",
+// description:'The e-commerce application offers a seamless platform for consumers to effortlessly purchase books. With the ability to search and filter books by category, users can easily find their desired titles. Furthermore, they can conveniently add books to their cart and wish list, proceed to checkout, and review their orders. To enhance the overall experience, customers receive email notifications once their orders have been successfully placed. On the administrative side, the application equips administrators with efficient product and user management functionalities, allowing them to effectively oversee and manage the platform operations. Tech Stack: The application is built using a robust tech stack, including React for the frontend, Node.js and Express for the backend, PotgreSQL for the database, and Node Mailer for email notifications.'
+// },
 ];
 
 const recentWork = [
@@ -37,7 +37,7 @@ const recentWorkDisplay = recentWork.map((work) => {
       <div key={work.name} className="bg-blue-500 p-4 rounded-lg shadow-md mb-4 hover:bg-blue-600 hover:shadow-lg transition-all duration-300">
         <div className="flex-col items-center justify-between mb-4">
           <div>
-            <a className="text-white text-2xl font-semibold" href={work.to} target="_blank" >{work.name}</a>
+            <a className="text-white text-2xl font-semibold" href={work.to} target="_blank" rel="noreferrer" >{work.name}</a>
             <div className="flex items-center mt-2 mb-2">
               <FiCode className="text-white text-xl mr-2" />
               <p className="text-white text-sm">Tech Stack: React, Node.js, Express</p>
@@ -47,8 +47,8 @@ const recentWorkDisplay = recentWork.map((work) => {
             work.video ? (<div>
               <video className="rounded-md" src={work.video} controls />
             </div>):(
-              <div target="_blank" href={work.to}>
-              <a className="rounded-md" target="_blank" href={work.to}> {work.to} </a>
+              <div target="_blank" rel="noreferrer" href={work.to}>
+              <a className="rounded-md" target="_blank" rel="noreferrer" href={work.to}> {work.to} </a>
             </div>
             )
           }
