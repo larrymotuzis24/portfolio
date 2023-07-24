798c6862ea8c61526d4dd98375c8a2396b751a5e
Larry Motuzis <larrymotuzis@Larrys-MacBook-Pro.local>
2023-07-23 21:10:45 -0500

    updated

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,8 +7,9 @@ const Projects = () => {
 const prevProjects = [
 {
 name: "Test Brew",
-to: "/",
+to: "https://testbrew.herokuapp.com/",
 video: testBrewDemo,
+techStack:'React, JavaScript, Express, Webpack, Node.js, SQL, TailWind CSS, JSON Web Token',
 description:'TestBrew is a platform that empowers developers to enhance their unit testing skills in just 15 minutes. By engaging in a series of exercises, users can learn to write more effective unit tests. TestBrew evaluates the unit tests written by users by utilizing the abstract syntax tree parsing capabilities of Acorn/Acorn-Walk. This analysis enables TestBrew to provide valuable suggestions and hints to users, helping them improve and rectify their unit tests. The frontend of TestBrew was created using Codemirror and React, ensuring a seamless and user-friendly interface. On the other hand, the backend was developed utilizing Node.js and Express, ensuring a robust and efficient foundation for the platform.'
 },
 // {
@@ -22,11 +23,13 @@ const recentWork = [
   {
     name: "TIC-TAC-TOE",
     to: "https://larrymotuzis24.github.io/tic-tac-toe/",
+    techStack:'React, JavaScript, TailWind CSS',
     description: 'Created a classic Tic-Tac-Toe game using Tic-Tac-Toe app using React.js. The app allows users to play the classic game against an AI opponent. It features an intuitive user interface, responsive design, and smooth gameplay experience. The app incorporates a range of technologies and concepts, including React hooks, Redux for state management, and the Minimax algorithm for the AI opponent. The Minimax algorithm ensures that the AI opponent makes optimal moves, providing a challenging experience for players.'
   },  
   {
   name: "CVG Website",
   to: "https://www.coopervaluationgroup.com/",
+  techStack:'React, JavaScript, TailWind CSS',
   // video: CVGDemo,
   description:'I collaborated with an appraisal group to develop a dynamic and user-friendly website that caters to their specific needs. The website serves as a comprehensive platform for the group, offering valuable features and functionalities to enhance their operations.'
   }
@@ -40,7 +43,7 @@ const recentWorkDisplay = recentWork.map((work) => {
             <a className="text-white text-2xl font-semibold" href={work.to} target="_blank" rel="noreferrer" >{work.name}</a>
             <div className="flex items-center mt-2 mb-2">
               <FiCode className="text-white text-xl mr-2" />
-              <p className="text-white text-sm">Tech Stack: React, Node.js, Express</p>
+              <p className="text-white text-sm">Tech Stack: {work.techStack}</p>
             </div>
           </div>
           {
@@ -69,7 +72,7 @@ const recentWorkDisplay = recentWork.map((work) => {
             <h4 className="text-white text-2xl font-semibold">{proj.name}</h4>
             <div className="flex items-center mt-2 mb-2">
               {proj.video ? <FiCode className="text-white text-xl mr-2" /> : <FiBookOpen className="text-white text-xl mr-2" />}
-              <p className="text-white text-sm">Tech Stack: React, Node.js, Express</p>
+              <p className="text-white text-sm">Tech Stack: {proj.techStack} </p>
             </div>
           </div>
           <div>
